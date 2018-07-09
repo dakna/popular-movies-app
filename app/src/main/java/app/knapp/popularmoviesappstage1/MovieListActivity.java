9f1ecdc10a2f4ea05491d81990eee3db48224b8c
Daniel Knapp <opendroid@gmail.com>
2018-07-09 14:42:39 -0400

    init skeleton app

---
+++ app/src/main/java/app/knapp/popularmoviesappstage1/MovieListActivity.java
@@ -0,0 +1,13 @@
+package app.knapp.popularmoviesappstage1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
