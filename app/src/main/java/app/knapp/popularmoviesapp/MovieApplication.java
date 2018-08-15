9024cf69dc565e672142ab98a4f44eb17ad38319
Daniel Knapp <opendroid@gmail.com>
2018-08-15 17:12:10 -0400

    add stetho debug bridge

---
+++ app/src/main/java/app/knapp/popularmoviesapp/MovieApplication.java
@@ -0,0 +1,14 @@
+package app.knapp.popularmoviesapp;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class MovieApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Stetho.initializeWithDefaults(this);
+    }
+}
